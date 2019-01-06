8633149cf26c3c87a68716fcb93c4749fe82dae1
Paul Gualotuna <paul.gualotuna@tum.de>
2019-01-06 21:07:18 +0100

    Pass properties to CommentDetails

--- src/index.js
+++ src/index.js
@@ -6,11 +6,14 @@ import CommentDetail from './CommentDetail';
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail/>
-            <CommentDetail/>
-            <CommentDetail/>
-            <CommentDetail/>
-            <CommentDetail/>
+            <CommentDetail
+                author="Sam"
+                timeAgo="Today at 4:45PM"
+                comment="hey hey"
+                avatar={faker.image.avatar}
+            />
+            <CommentDetail author="Alex" timeAgo="Today at 2:00PM" comment="Hallo" avatar={faker.image.avatar}/>
+            <CommentDetail author="Jane" timeAgo="Today at 5:45PM" comment="Nice Blog!" avatar={faker.image.avatar}/>
         </div>
     );
 };
